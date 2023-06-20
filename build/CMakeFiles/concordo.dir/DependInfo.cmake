de6f18e237aaa4399f761ee0005e17bfe6535495
Calafange <jotave369@gmail.com>
2023-06-20 13:46:24 -0300

    concordo em funcionamento, faltando readme e documentação em doxygem

---
+++ build/CMakeFiles/concordo.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/jotav/Área de Trabalho/mySpace/cppSidemar/Concordo_LP1/src/channel.cpp" "/mnt/c/Users/jotav/Área de Trabalho/mySpace/cppSidemar/Concordo_LP1/build/CMakeFiles/concordo.dir/src/channel.cpp.o"
+  "/mnt/c/Users/jotav/Área de Trabalho/mySpace/cppSidemar/Concordo_LP1/src/main.cpp" "/mnt/c/Users/jotav/Área de Trabalho/mySpace/cppSidemar/Concordo_LP1/build/CMakeFiles/concordo.dir/src/main.cpp.o"
+  "/mnt/c/Users/jotav/Área de Trabalho/mySpace/cppSidemar/Concordo_LP1/src/message.cpp" "/mnt/c/Users/jotav/Área de Trabalho/mySpace/cppSidemar/Concordo_LP1/build/CMakeFiles/concordo.dir/src/message.cpp.o"
+  "/mnt/c/Users/jotav/Área de Trabalho/mySpace/cppSidemar/Concordo_LP1/src/server.cpp" "/mnt/c/Users/jotav/Área de Trabalho/mySpace/cppSidemar/Concordo_LP1/build/CMakeFiles/concordo.dir/src/server.cpp.o"
+  "/mnt/c/Users/jotav/Área de Trabalho/mySpace/cppSidemar/Concordo_LP1/src/sistema.cpp" "/mnt/c/Users/jotav/Área de Trabalho/mySpace/cppSidemar/Concordo_LP1/build/CMakeFiles/concordo.dir/src/sistema.cpp.o"
+  "/mnt/c/Users/jotav/Área de Trabalho/mySpace/cppSidemar/Concordo_LP1/src/textChannel.cpp" "/mnt/c/Users/jotav/Área de Trabalho/mySpace/cppSidemar/Concordo_LP1/build/CMakeFiles/concordo.dir/src/textChannel.cpp.o"
+  "/mnt/c/Users/jotav/Área de Trabalho/mySpace/cppSidemar/Concordo_LP1/src/user.cpp" "/mnt/c/Users/jotav/Área de Trabalho/mySpace/cppSidemar/Concordo_LP1/build/CMakeFiles/concordo.dir/src/user.cpp.o"
+  "/mnt/c/Users/jotav/Área de Trabalho/mySpace/cppSidemar/Concordo_LP1/src/voiceChannel.cpp" "/mnt/c/Users/jotav/Área de Trabalho/mySpace/cppSidemar/Concordo_LP1/build/CMakeFiles/concordo.dir/src/voiceChannel.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/concordo.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/concordo.dir/src/channel.cpp.o"
+  "CMakeFiles/concordo.dir/src/main.cpp.o"
+  "CMakeFiles/concordo.dir/src/message.cpp.o"
+  "CMakeFiles/concordo.dir/src/server.cpp.o"
+  "CMakeFiles/concordo.dir/src/sistema.cpp.o"
+  "CMakeFiles/concordo.dir/src/textChannel.cpp.o"
+  "CMakeFiles/concordo.dir/src/user.cpp.o"
+  "CMakeFiles/concordo.dir/src/voiceChannel.cpp.o"
+  "concordo"
+  "concordo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/concordo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
